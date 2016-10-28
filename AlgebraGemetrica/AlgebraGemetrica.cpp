ab7dad76d7fd7de50e2ccdb4eac5162d3370feb3
altobellibm <altobelli.bm@gmail.com>
2016-10-28 11:37:01 -0200

    usando IDE visual studio

--- AlgebraGemetrica/AlgebraGemetrica.cpp
+++ AlgebraGemetrica/AlgebraGemetrica.cpp
@@ -7,7 +7,7 @@
 typedef std::vector<Multivector<int>> BASE_BLADE;
 typedef std::vector<std::vector<Multivector<int>>> CLIFFORD_TABLE;
 
-enum class OPERATION {GP, SCP, LCONST, RCONST};
+enum class OPERATION { GP, SCP, LCONST, RCONST };
 
 void createCliffordTable(const BASE_BLADE& _bladesBase, const Orthonormal& ortonormal, OPERATION _type, CLIFFORD_TABLE& _table){
 
@@ -16,16 +16,15 @@ void createCliffordTable(const BASE_BLADE& _bladesBase, const Orthonormal& orton
    for(BASE_BLADE::size_type i = 0; i < _bladesBase.size(); i++)
       for(BASE_BLADE::size_type j = 0; j < _bladesBase.size(); j++){
           switch (_type) {
-          case OPERATION::GP: _table[i][j] = GP(_bladesBase[i],bladesBase[j],ortonormal); break;
-          case OPERATION::SCP: _table[i][j] = SCP(_bladesBase[i],bladesBase[j],ortonormal); break;
-          case OPERATION::LCONST: _table[i][j] = LConst(_bladesBase[i],bladesBase[j],ortonormal); break;
-          case OPERATION::RCONST: _table[i][j] = RConst(_bladesBase[i],bladesBase[j],ortonormal); break;
+          case OPERATION::GP: _table[i][j] = GP(_bladesBase[i], _bladesBase[j],ortonormal); break;
+          case OPERATION::SCP: _table[i][j] = SCP(_bladesBase[i], _bladesBase[j],ortonormal); break;
+          case OPERATION::LCONST: _table[i][j] = LConst(_bladesBase[i], _bladesBase[j],ortonormal); break;
+          case OPERATION::RCONST: _table[i][j] = RConst(_bladesBase[i], _bladesBase[j],ortonormal); break;
           default:
               break;
           }
       }
 }
-
 
 int main(int , char** )
 {
@@ -107,9 +106,10 @@ int main(int , char** )
             // dúvida
 
      // Exercício 7
+
       BASE_BLADE bladesBase = {(e(0)),(e(1)),(e(2)),(e(3)), (e(1)^e(2)), (e(1)^e(3)), (e(2)^e(3)),(e(1)^e(2)^e(3))};
 
-      CLIFFORD_TABLE tableGP;
+	  CLIFFORD_TABLE tableGP;
       createCliffordTable(bladesBase, ortonormal, OPERATION::GP, tableGP);
 
       CLIFFORD_TABLE tableSCP;
@@ -120,8 +120,7 @@ int main(int , char** )
 
       CLIFFORD_TABLE tableRCONST;
       createCliffordTable(bladesBase, ortonormal, OPERATION::RCONST, tableRCONST);
-
-
+	  
     // Exercício 8
 
       // Não tem como executar esse exercício.
